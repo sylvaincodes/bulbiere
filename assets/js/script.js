a27596c2dfd08428be99eded21b10047d11a1c92
sylvaincodes <adandesylvain2010@gmail.com>
2022-10-10 16:23:17 +0200

    trendinngs products section end home page 1200px

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
+/*variables*/
+const div_products = document.getElementById('products');
+
+
+/* 
+    *initailiser le carousel slider swipper 
+    *page d'acceuil
+*/
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
   
+ 
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
@@ -19,3 +28,60 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+
+/* 
+    * animation fade
+    * supprimer un div de la cart 
+    * cart
+*/
+function removeListItem(e) {
+  let parent = e.parentElement; 
+  parent.classList.add('fade');
+  setTimeout(() => {
+    parent.remove();
+  }, 1000);
+}
+
+
+/* 
+    * ajout d'un article au panier
+    * css 
+    
+*/
+function addFillCss(e) {
+  e.classList.add('fill_in');
+}
+
+/* 
+    * scroll to the left the trending product
+    * home page
+    
+*/
+/*
+const slider = document.querySelector('.products');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});*/

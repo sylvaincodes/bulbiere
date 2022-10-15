6228308e17e9b612b5448b3faf05703debfdec66
sylvaincodes <adandesylvain2010@gmail.com>
2022-10-15 03:58:07 +0200

    product details page max 1200px

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
-/*variables*/
+
+  /*variables*/
 const div_products = document.getElementById('products');
+const color = document.getElementsByClassName('color');
+const dec = document.getElementById('dec');
+const inc = document.getElementById('inc');
+const qty = document.getElementById('qty');
 
 
 /* 
@@ -45,9 +50,9 @@ function removeListItem(e) {
 
 /* 
     * ajout d'un article au panier
-    * css 
-    
+    * css  
 */
+
 function addFillCss(e) {
   e.classList.add('fill_in');
 }
@@ -75,3 +80,92 @@ window.onscroll = function(){
   }
 }
 
+/* 
+    * quantity increase or decrease
+    * product details page    
+*/
+
+dec.onclick =  decreseQty;
+inc.onclick =  increaseQty;
+
+function decreseQty(){
+
+  if (qty.value==1) {
+    
+  }else{
+    qty.value = --qty.value;
+  }
+}
+
+function increaseQty(){
+  qty.value = ++qty.value;
+}
+
+
+/*--
+    Magnific Popup
+    ------------------------*/
+    $('.img-popup').magnificPopup({
+      type: 'image',
+      gallery: {
+          enabled: true
+      }
+  });
+
+/*
+  * set Activ image on produit single
+  * Product single
+  * */
+function setActiveImage(id) {
+
+  document.querySelectorAll('.image-box').forEach(element => {
+    
+    element.classList.remove('active');
+
+  }); 
+  
+  document.querySelectorAll('.image-gallery img').forEach(element => {
+    
+    element.classList.remove('active');
+
+  });  
+  
+  document.querySelectorAll('.'+id).forEach(element => {
+    
+    element.classList.add('active');
+
+  });
+
+}
+
+
+/*
+  * set Activ tab on produit single
+  * Product single
+  * */
+function setActiveTab(id) {
+
+  document.querySelectorAll('.tab-link').forEach(element => {
+    
+    element.classList.remove('active');
+
+  }); 
+  
+  document.querySelectorAll('.tab').forEach(element => {
+    
+    element.classList.remove('active');
+
+  });  
+  
+  document.querySelectorAll('.'+id).forEach(element => {
+    
+    element.classList.add('active');
+
+  });
+
+}
+
+
+$(':radio').change(function() {
+  console.log('Note : ' + this.value);
+});
